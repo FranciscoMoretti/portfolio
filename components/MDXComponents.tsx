11066a5f4ad564b6ca136c3100b0137884906c16
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-16 12:13:08 -0300

    remove unused metrics components

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -3,10 +3,6 @@ import Image from 'next/image';
 
 import ProsCard from 'components/ProsCard';
 import ConsCard from 'components/ConsCard';
-import Gumroad from 'components/metrics/Gumroad';
-import Unsplash from 'components/metrics/Unsplash';
-import Analytics from 'components/metrics/Analytics';
-import YouTube from 'components/metrics/Youtube';
 import Step from 'components/Step';
 import ImageWithTheme from 'components/ImageWithTheme';
 
@@ -33,13 +29,9 @@ const MDXComponents = {
   Image: RoundedImage,
   ImageWithTheme,
   a: CustomLink,
-  Analytics,
   ConsCard,
-  Gumroad,
   ProsCard,
-  Step,
-  Unsplash,
-  YouTube
+  Step
 };
 
 export default MDXComponents;
