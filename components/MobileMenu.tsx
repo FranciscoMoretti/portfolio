312a45f6894c5a859f7f4e29f85f8385b5a7fe07
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-16 12:01:28 -0300

    Renove newsletter infrastructure and its pages

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -83,14 +83,6 @@ export default function MobileMenu() {
           </li>
           <li
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
-            style={{ transitionDelay: '300ms' }}
-          >
-            <Link href="/newsletter">
-              <a className="flex w-auto pb-4">Newsletter</a>
-            </Link>
-          </li>
-          <li
-            className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '325ms' }}
           >
             <Link href="/tweets">
