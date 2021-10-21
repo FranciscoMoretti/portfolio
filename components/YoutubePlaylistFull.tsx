15b8849dcc7ae8ffdb6746e34b19ef41f5507aec
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-21 14:46:44 -0300

    VideoCardFull and YoutubePlaylistFull components

---
+++ components/YoutubePlaylistFull.tsx
@@ -0,0 +1,30 @@
+import useSWR from 'swr';
+
+import fetcher from 'lib/fetcher';
+import { YouTubePlaylist} from 'lib/types';
+import VideoCardFull from 'components/VideoCardFull';
+
+export default function YouTubePlaylistFull() {
+  const { data } = useSWR<YouTubePlaylist>('/api/youtube', fetcher);
+
+  return (
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-2 w-full">
+      {!data && (
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
+          No videos found.
+        </p>
+      )}
+      {data && data.map( youTubeVideo => (
+        <VideoCardFull
+          key={youTubeVideo.position}
+          title={youTubeVideo.title}
+          href={'https://www.youtube.com/watch?v='+ youTubeVideo.videoId}
+          channel={youTubeVideo.videoOwnerChannelTitle}
+          index={youTubeVideo.position}
+          thumbnail={youTubeVideo.thumbnail}
+        />
+      )
+      )}
+    </div>
+  );
+}
