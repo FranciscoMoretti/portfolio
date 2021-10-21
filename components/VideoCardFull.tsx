df2b72cc282490aec3be9411b51f836372ad8b5e
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-21 15:58:11 -0300

    Refinement of VideoCardFull layout

--- components/VideoCardFull.tsx
+++ components/VideoCardFull.tsx
@@ -7,19 +7,19 @@ export default function VideoCardFull({ href, channel, title, index, thumbnail }
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="w-full border-b border-gray-200 dark:border-gray-700 py-3 transform hover:scale-[1.01] transition-all">
-        <div className="flex flex-col justify-between items-center">
+      <div className="metric-card bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-lg p-4 max-w-72 w-full  h-full py-3 transform hover:scale-[1.01] transition-all">
+        <div className="flex flex-col justify-between items-center h-full">
           <div className="flex flex-col items-center">
             <img width={thumbnail.width} height={thumbnail.height} src={thumbnail.url} alt="" />
-            <h4 className="text-lg font-medium w-full text-gray-800 dark:text-gray-100">
+            <h4 className="youtube-video-legend text-lg font-medium w-full text-gray-800 dark:text-gray-100">
               {title}
             </h4>
-            <div className="text-gray-300 dark:text-gray-400 text-left self-start">
+            <div className="youtube-video-legend text-gray-300 dark:text-gray-400 text-left self-start">
               {channel}
             </div>
           </div>
-          <div className="flex items-center flex-rowmt-2 sm:mt-0 w-full sm:w-auto justify-end	self-end">
-            <p className="text-gray-500 dark:text-gray-400 text-left sm:text-right w-32 md:mb-0 mr-2 ml-10 sm:ml-0">
+          <div className="flex items-center flex-row mt-2 sm:mt-0 w-full sm:w-auto justify-end	self-end">
+            <p className="text-gray-500 dark:text-gray-400 text-left sm:text-right md:mb-0 mr-2 ml-10 sm:ml-0">
               Watch on YouTube
             </p>
             <svg
