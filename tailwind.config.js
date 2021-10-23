e92fc02a7fc04a54815bfed5b9a7b96021a32c36
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-23 17:09:26 -0300

    bug-fix apply theme to h1 titles as well

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
               },
               code: { color: theme('colors.blue.400') }
             },
-            'h2,h3,h4': {
+            'h1,h2,h3,h4': {
               'scroll-margin-top': spacing[32]
             },
             thead: {
@@ -60,7 +60,7 @@ module.exports = {
               borderLeftColor: theme('colors.gray.700'),
               color: theme('colors.gray.300')
             },
-            'h2,h3,h4': {
+            'h1,h2,h3,h4': {
               color: theme('colors.gray.100'),
               'scroll-margin-top': spacing[32]
             },
