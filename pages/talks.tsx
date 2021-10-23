3256e2636fa322575500f8e861520409577488e9
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-23 13:24:53 -0300

    Add Watch all videos link in talks page

--- pages/talks.tsx
+++ pages/talks.tsx
@@ -25,6 +25,28 @@ export default function SoftwareTalks({
         <div className="flex flex-col w-full">
           <YoutubePlaylistFull posts={posts} />
         </div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://youtube.com/playlist?list=PLeEX4sJL6yWxWPuKUS9gEl5RAqkX6e-kS"
+          className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+        >
+          Watch all videos
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 ml-1"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+            />
+          </svg>
+        </a>
       </div>
     </Container>
   );
