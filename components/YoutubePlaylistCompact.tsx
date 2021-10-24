2d1a5a9d9b31262f882bf39f019d8b4915bd9254
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-24 20:10:12 -0300

    Re-factor YoutubePlaylistCompact content

--- components/YoutubePlaylistCompact.tsx
+++ components/YoutubePlaylistCompact.tsx
@@ -4,26 +4,41 @@ import fetcher from 'lib/fetcher';
 import { YouTubePlaylist} from 'lib/types';
 import VideoCardCompact from '../components/VideoCardCompact';
 
+function RenderPlaylist({data, maxItems}): JSX.Element{
+  return <>{
+    data.slice(0, Number(maxItems)).map( youTubeVideo => (
+      <VideoCardCompact
+        key={youTubeVideo.position}
+        title={youTubeVideo.title}
+        href={'https://www.youtube.com/watch?v='+ youTubeVideo.videoId}
+        channel={youTubeVideo.videoOwnerChannelTitle}
+        index={youTubeVideo.position + 1} // Start from index 1
+      />
+    )
+    )
+  }</>
+}
+
+function RenderPlaylistIfReady({data, maxItems}){
+  if (!data){
+    return (
+      <p className="mb-4 text-gray-600 dark:text-gray-400">
+        Loading playlist...
+      </p>
+    )
+  }
+
+  return (
+    <RenderPlaylist data={data} maxItems={maxItems}/>
+  )
+}
+
 export default function YouTubePlaylistCompact({ maxItems }) {
   const { data } = useSWR<YouTubePlaylist>('/api/youtube', fetcher);
 
   return (
     <div>
-      {!data && (
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          No videos found.
-        </p>
-      )}
-      {data && data.slice(0, Number(maxItems)).map( youTubeVideo => (
-        <VideoCardCompact
-          key={youTubeVideo.position}
-          title={youTubeVideo.title}
-          href={'https://www.youtube.com/watch?v='+ youTubeVideo.videoId}
-          channel={youTubeVideo.videoOwnerChannelTitle}
-          index={youTubeVideo.position + 1} // Start from index 1
-        />
-      )
-      )}
+      <RenderPlaylistIfReady data={data} maxItems={maxItems}/>
     </div>
   );
 }
