e0a8375c0def4145d9b83c023ff4714e001a65ac
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-24 20:02:54 -0300

    Re-factor YoutubePlaylistFull content

--- components/YoutubePlaylistFull.tsx
+++ components/YoutubePlaylistFull.tsx
@@ -4,6 +4,44 @@ import fetcher from 'lib/fetcher';
 import { YouTubePlaylist} from 'lib/types';
 import VideoCardFull from 'components/VideoCardFull';
 
+function RenderPlaylist({data, postOfVideoId}): JSX.Element{
+  return <>{
+    data.map( youTubeVideo => {
+    const videoLink = 'https://www.youtube.com/watch?v=' + youTubeVideo.videoId
+    const notesAreAvailable = youTubeVideo.videoId in postOfVideoId
+    let href = notesAreAvailable ?
+      'talk/'+ youTubeVideo.videoId :
+      videoLink
+
+    return (
+      <VideoCardFull
+        key={youTubeVideo.position}
+        title={youTubeVideo.title}
+        href={href}
+        channel={youTubeVideo.videoOwnerChannelTitle}
+        index={youTubeVideo.position}
+        thumbnail={youTubeVideo.thumbnail}
+        notesAreAvailable={notesAreAvailable}
+      />
+      )
+    })
+  }</>
+}
+
+function RenderPlaylistIfReady({data, postOfVideoId}){
+  if (!data){
+    return (
+      <p className="mb-4 text-gray-600 dark:text-gray-400">
+        Loading playlist...
+      </p>
+    )
+  }
+
+  return (
+    <RenderPlaylist data={data} postOfVideoId={postOfVideoId}/>
+  )
+}
+
 export default function YouTubePlaylistFull({ posts }) {
   const { data } = useSWR<YouTubePlaylist>('/api/youtube', fetcher);
 
@@ -12,31 +50,7 @@ export default function YouTubePlaylistFull({ posts }) {
 
   return (
     <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-2 w-full">
-      {!data && (
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          Loading playlist...
-        </p>
-      )}
-      {data && data.map( youTubeVideo => {
-        const videoLink = 'https://www.youtube.com/watch?v=' + youTubeVideo.videoId
-        const notesAreAvailable = youTubeVideo.videoId in postOfVideoId
-        let href = notesAreAvailable ?
-          'talk/'+ youTubeVideo.videoId :
-          videoLink
-
-        return (
-        <VideoCardFull
-          key={youTubeVideo.position}
-          title={youTubeVideo.title}
-          href={href}
-          channel={youTubeVideo.videoOwnerChannelTitle}
-          index={youTubeVideo.position}
-          thumbnail={youTubeVideo.thumbnail}
-          notesAreAvailable={notesAreAvailable}
-        />
-        )
-    }
-      )}
+      <RenderPlaylistIfReady data={data} postOfVideoId={postOfVideoId}/>
     </div>
   );
 }
