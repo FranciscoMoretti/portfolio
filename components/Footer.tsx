1b49ca48150c79b3528939e1665e1312e68dc994
Francisco Moretti <franciscoemoretti@gmail.com>
2021-10-27 11:04:04 -0300

    Add Footer note to mention the original repo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,6 +47,15 @@ export default function Footer() {
           </Link>
         </div>
       </div>
+      <div className="flex flex-col w-full items-center space-y-4">
+        <p className="text-gray-500">
+          The backbone of this site is a fork of&nbsp;
+          <ExternalLink href="https://github.com/leerob/leerob.io">
+            <a className="hover:text-gray-600 transition">leerob.io</a>
+          </ExternalLink>
+          &nbsp;built by @leeerob.
+        </p>
+      </div>
     </footer>
   );
 }
