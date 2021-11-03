ca03d9f1379594427f1214cd26882b0eb0a0760b
Francisco Moretti <franciscoemoretti@gmail.com>
2021-11-03 00:09:06 +0000

    change dark-light button proportions

--- components/Container.tsx
+++ components/Container.tsx
@@ -86,7 +86,7 @@ export default function Container(props) {
           <button
             aria-label="Toggle Dark Mode"
             type="button"
-            className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+            className="w-16 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
             onClick={() =>
               setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
             }
