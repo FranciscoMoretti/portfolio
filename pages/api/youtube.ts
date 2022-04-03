dbc941e9a9b05df31bb6dbff49e64b2807d59853
FranciscoMoretti <franciscoemoretti@gmail.com>
2022-04-03 09:37:25 +0000

    bugfix only handle results with thumbnails

--- pages/api/youtube.ts
+++ pages/api/youtube.ts
@@ -28,14 +28,17 @@ export default async function handler(
     const videoId: string = items[i].contentDetails.videoId;
     const title: string = items[i].snippet.title;
     const videoOwnerChannelTitle: string = items[i].snippet.videoOwnerChannelTitle;
-    const thumbnail: YouTubeVideoThumbnail = {
-      url:items[i].snippet.thumbnails.medium.url,
-      width:items[i].snippet.thumbnails.medium.width,
-      height:items[i].snippet.thumbnails.medium.height,
-    };
-    const position: number = items[i].snippet.position;
-    const youtubeVideo: YouTubeVideo = {videoId, title, videoOwnerChannelTitle, thumbnail, position}
-    youTubePlaylist.push(youtubeVideo)
+    const medium_thumbnail = items[i].snippet.thumbnails.medium
+    if (medium_thumbnail != undefined){
+      const thumbnail: YouTubeVideoThumbnail = {
+        url:medium_thumbnail.url,
+        width:medium_thumbnail.width,
+        height:medium_thumbnail.height,
+      };
+      const position: number = items[i].snippet.position;
+      const youtubeVideo: YouTubeVideo = {videoId, title, videoOwnerChannelTitle, thumbnail, position}
+      youTubePlaylist.push(youtubeVideo)
+    }
   };
 
   res.setHeader(
