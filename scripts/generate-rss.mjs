978019c547f2fc6d806fdfe513d66a4e4f0785cb
FranciscoMoretti <franciscoemoretti@gmail.com>
2022-04-14 11:38:45 +0000

    bug-fix: outdated import path in script

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import RSS from 'rss';
-import { allBlogPostContents } from 'contentlayer/generated';
+import { allBlogPostContents } from  '../.contentlayer/generated/index.mjs';
 
 async function generate() {
   const feed = new RSS({
