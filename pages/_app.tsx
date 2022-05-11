3136823df7ea09ced4b0987283e757ef21a868c4
Francisco Moretti <franciscoemoretti@gmail.com>
2022-05-11 11:18:45 +0100

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,49 @@ import 'styles/global.css';
 
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-export default function App({ Component, pageProps }: AppProps) {
-
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
     <ThemeProvider attribute="class">
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
+
+export default App
